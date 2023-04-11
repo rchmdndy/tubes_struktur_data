5f0f951e0d0d7c744bef86c4861426aafe3bd07d
rachmadandy <rachmadandymahendras@gmail.com>
2023-04-11 19:15:42 +0700

    pembuuatan class data dan elemen

---
+++ src/main/java/org/example/data.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class data {
+    String nama, penulis, penerbit;
+    int harga;
+
+    public data(String nama, String penulis, String penerbit, int harga) {
+        this.nama = nama;
+        this.penulis = penulis;
+        this.penerbit = penerbit;
+        this.harga = harga;
+    }
+}
---
+++ src/main/java/org/example/elemen.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class elemen {
+
+    elemen next;
+    elemen prev;
+    elemen (String nama, String penulis, String penerbit, int harga){
+        info = new data(nama, penulis, penerbit, harga);
+        next = null;
+        prev = null;
+    }
+
+    data info;
+
+
+}
