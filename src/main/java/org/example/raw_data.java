46cb6534b1ce6a8d7dbff2c02c67d6c94a2b8c16
rachmadandy <rachmadandymahendras@gmail.com>
2023-04-16 20:47:38 +0700

    membuat class raw_data

---
+++ src/main/java/org/example/raw_data.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class raw_data {
+   data buku1 = new data("Harry Potter and the Philosopher's Stone", 
+                              "J.K. Rowling", 
+                              "Bloomsbury Publishing", 
+                              150000);
+        
+        data buku2 = new data("The Hobbit", 
+                              "J.R.R. Tolkien", 
+                              "Houghton Mifflin", 
+                              120000);
+        
+        data buku3 = new data("To Kill a Mockingbird", 
+                              "Harper Lee", 
+                              "J.B. Lippincott & Co.", 
+                              90000);
+        
+        data buku4 = new data("1984", 
+                              "George Orwell", 
+                              "Secker & Warburg", 
+                              80000);
+        
+        data buku5 = new data("Pride and Prejudice", 
+                              "Jane Austen", 
+                              "T. Egerton, Whitehall", 
+                              75000);
+        
+        data buku6 = new data("The Catcher in the Rye", 
+                              "J.D. Salinger", 
+                              "Little, Brown and Company", 
+                              95000);
+        
+        data buku7 = new data("Animal Farm", 
+                              "George Orwell", 
+                              "Secker & Warburg", 
+                              85000);
+        
+        data buku8 = new data("The Great Gatsby", 
+                              "F. Scott Fitzgerald", 
+                              "Charles Scribner's Sons", 
+                              110000);
+        
+        data buku9 = new data("One Hundred Years of Solitude", 
+                              "Gabriel García Márquez", 
+                              "Harper & Row", 
+                              135000);
+        
+        data buku10 = new data("Brave New World", 
+                               "Aldous Huxley", 
+                               "Chatto & Windus", 
+                               95000);
+   
+}
