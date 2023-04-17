0bfda702b30dffbabb3906555222bc437696ce54
davin <davinfausta@outlook.co.id>
2023-04-17 21:03:56 +0700

    bang itu raw_datanya

--- src/main/java/org/example/raw_data.java
+++ src/main/java/org/example/raw_data.java
@@ -1,102 +1,139 @@
 package org.example;
 
 public class raw_data {
-   data buku1 = new data("Harry Potter and the Philosopher's Stone", 
+   void fiksi(){
+      data fiksi1 = new data("Harry Potter and the Philosopher's Stone", 
                               "J.K. Rowling", 
                               "Bloomsbury Publishing", 
                               150000);
         
-        data buku2 = new data("The Hobbit", 
-                              "J.R.R. Tolkien", 
-                              "Houghton Mifflin", 
-                              120000);
+      data fiksi2 = new data("The Hobbit", 
+                        "J.R.R. Tolkien", 
+                        "Houghton Mifflin", 
+                        120000);
         
-        data buku3 = new data("To Kill a Mockingbird", 
+      data fiksi3 = new data("To Kill a Mockingbird", 
                               "Harper Lee", 
                               "J.B. Lippincott & Co.", 
                               90000);
         
-        data buku4 = new data("1984", 
+      data fiksi4 = new data("1984", 
                               "George Orwell", 
                               "Secker & Warburg", 
                               80000);
         
-        data buku5 = new data("Pride and Prejudice", 
+      data fiksi5 = new data("Pride and Prejudice", 
                               "Jane Austen", 
                               "T. Egerton, Whitehall", 
                               75000);
         
-        data buku6 = new data("The Catcher in the Rye", 
+      data fiksi6 = new data("The Catcher in the Rye", 
                               "J.D. Salinger", 
                               "Little, Brown and Company", 
                               95000);
         
-        data buku7 = new data("Animal Farm", 
+      data fiksi7 = new data("Animal Farm", 
                               "George Orwell", 
                               "Secker & Warburg", 
                               85000);
         
-        data buku8 = new data("The Great Gatsby", 
+      data fiksi8 = new data("The Great Gatsby", 
                               "F. Scott Fitzgerald", 
                               "Charles Scribner's Sons", 
                               110000);
         
-        data buku9 = new data("One Hundred Years of Solitude", 
+      data fiksi9 = new data("One Hundred Years of Solitude", 
                               "Gabriel García Márquez", 
                               "Harper & Row", 
                               135000);
         
-        data buku10 = new data("Brave New World", 
+      data fiksi10 = new data("Brave New World", 
                                "Aldous Huxley", 
                                "Chatto & Windus", 
                                95000);
-                               data buku11 = new data("The Lord of the Rings", 
-                               "J.R.R. Tolkien", 
-                               "George Allen & Unwin", 
-                               180000);
-        
-        data buku12 = new data("The Da Vinci Code", 
-                               "Dan Brown", 
-                               "Doubleday", 
-                               95000);
-        
-        data buku13 = new data("The Hunger Games", 
-                               "Suzanne Collins", 
-                               "Scholastic", 
-                               75000);
-        
-        data buku14 = new data("The Alchemist", 
-                               "Paulo Coelho", 
-                               "HarperCollins", 
-                               85000);
-        
-        data buku15 = new data("The Chronicles of Narnia", 
-                               "C.S. Lewis", 
-                               "Geoffrey Bles", 
-                               130000);
-        
-        data buku16 = new data("The Girl with the Dragon Tattoo", 
-                               "Stieg Larsson", 
-                               "Norstedts Förlag", 
-                               120000);
-        
-        data buku17 = new data("The Fault in Our Stars", 
-                               "John Green", 
-                               "Dutton Books", 
-                               90000);
-        
-        data buku18 = new data("The Adventures of Sherlock Holmes", 
-                               "Arthur Conan Doyle", 
-                               "George Newnes Ltd", 
-                               80000);
-        
-        data buku19 = new data("The Picture of Dorian Gray", 
-                               "Oscar Wilde", 
-                               "Ward, Lock & Co", 
-                               75000);
-        
-        data buku20 = new data("The War of the Worlds", 
-                               "H.G. Wells", 
-                               "William Heinemann", 
-                               85000);
+   }
+   void pengetahuan(){
+      data pengetahuan1 = new data("Ensiklopedia Britanica", 
+                                    "Berbagai Penulis", 
+                                    "Ensiklopedia Britanicca Inc", 
+                                    675000);
+      data pengetahuan2 = new data("Altas Dunia", 
+                                 "Rand McNally", 
+                                 "Rand McNally & Company", 
+                                 2500000);
+      data pengetahuan3 = new data("Sapiens: A Brief History of Humankind", 
+                                    "Yuval Noah Harari", 
+                                    "Harper", 
+                                    194000);
+      data pengetahuan4 = new data("The Selfish Gene", 
+                                          "Richard Dawkins", 
+                                          "Oxford University Press",
+                                          90000);
+      data pengetahuan5 = new data("Guns, Germs, and Steel", 
+                                          "Jared Diamond", 
+                                          "W.W. Norton & Company",
+                                          549000);
+      data pengetahuan6 = new data("The Origin of Species", 
+                                          "Charles Darwin", 
+                                          "Penguin Classics",
+                                          200000);
+      data pengetahuan7 = new data("The Structure of Scientific Revolutions", 
+                                          "Thomas S. Kuhn", 
+                                          "University of Chicago Press",
+                                          57000);
+      data pengetahuan8 = new data("A Brief History of Time", 
+                                          "Stephen Hawking", 
+                                          "Bantam Books",
+                                          168000);
+      data pengetahuan9 = new data("Thinking, Fast and Slow", 
+                                 "Daniel Kahneman", 
+                                 "Farrar, Straus and Giroux", 
+                                 216000);
+      data pengetahuan10 = new data("The Feynman Lectures on Physics", 
+                                    "Richard P. Feynman", 
+                                    "Addison-Wesley",
+                                    3000000);
+   }
+   void religi(){
+      data religi1 = new data("Al-Qur'an", 
+                              "Allah SWT", 
+                              "Yayasan Literasi Quran Indonesia", 
+                              150000);
+      data religi2 = new data("Hadits Arbain", 
+                              "Imam Nawawi", 
+                              "Pustaka Al-Kautsar",
+                              66500);
+      data religi3 = new data("Muqaddimah", 
+                              "Ibnu Khaldun", 
+                              "Pustaka Arafah",
+                              400000);
+      data religi4 = new data("Fiqh Sirah", 
+                              "Syekh Yusuf Qardhawi", 
+                              "Gema Insani Press", 
+                              11000);
+      data religi5 = new data("Bidayatul Hidayah", 
+                              "Imam Al-Ghazali", 
+                              "Pustaka Al-Kautsar",
+                              60000);
+      data religi6 = new data("Majmu' Syarif", 
+                              "Imam Nawawi", 
+                              "Darul Haq", 
+                              100000);
+      data religi7 = new data("Tafsir al-Mishbah", 
+                              "M. Quraish Shihab", 
+                              "Lentera Hati", 
+                              79000);
+      data religi8 = new data("Mukasyafatul Qulub", 
+                              "Imam Al-Ghazali", 
+                              "Darul Falah", 
+                              167000);
+      data religi9 = new data("Kitab Kuning", 
+                              "Berbagai Penulis", 
+                              "Pustaka Al-Maktabah Al-Shamela", 
+                              199000);
+      data religi10 = new data("Hilyatul Auliya", 
+                              "Abu Nu'aim Al-Asfahani", 
+                              "Darul Kutub Al-Ilmiyyah", 
+                              215000);
+   }
 }
